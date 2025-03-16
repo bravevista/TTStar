3d76b3871ce4d85a4020a5227c3dd2d56a38dfc0
AlsoEdson <edsongutierrez789123@gmail.com>
2025-03-15 23:12:08 -0500

    Onboarding v0.2

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -3,8 +3,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import WelcomeScreen from '../screens/WelcomeScreen';
 import OnboardingScreen from '../screens/OnboardingScreen';
+import LoginScreen from '../screens/LoginScreen';
 
-const Stack = createStackNavigator();
+export type RootStackParamList = {
+    Onboarding: undefined;
+    Welcome: undefined;
+    Login: undefined;
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
 
 export const AppNavigator = () => {
     return (
@@ -14,6 +21,7 @@ export const AppNavigator = () => {
             }}>
                 <Stack.Screen name='Onboarding' component={OnboardingScreen} />
                 <Stack.Screen name='Welcome' component={WelcomeScreen} />
+                <Stack.Screen name='Login' component={LoginScreen} />
                 {/* Aquí agregarias el resto de pantallas */}
             </Stack.Navigator>
         </NavigationContainer>
--- src/screens/OnboardingScreen.tsx
+++ src/screens/OnboardingScreen.tsx
@@ -1,15 +1,78 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import Onboarding from '../components/specific/onboarding/Onboarding';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, useWindowDimensions } from 'react-native';
+import AppIntroSlider from 'react-native-app-intro-slider';
+import { Image } from 'expo-image';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+
+import slidesData from '../data/slides.data';
+import { useTheme } from '../hooks/useTheme';
+import { RootStackParamList } from '../navigation';
+import { NavigationButton } from '../components/specific/onboarding/NavigationButton';
+
+type typeButton = 'PrevButton' | 'SkipButton' | 'NextButton' | 'DoneButton';
+type NavigationProp = StackNavigationProp<RootStackParamList, 'Onboarding'>;
 
 export default function OnboardingScreen() {
-    return (
-        <View style={styles.container}>
-            <View>
-                <Onboarding />
-            </View>
-        </View>
+    const { colors, typography } = useTheme();
+    const { width, height } = useWindowDimensions();
+    const [showHomepage, setShowHomePage] = useState(false);
+    const navigation = useNavigation<NavigationProp>();
+
+    const buttonLabel = (label: string, type?: typeButton) => (
+        <NavigationButton
+            label={label}
+            type={type}
+            color={colors.primary}
+            fontSize={typography.fontSizes.sm}
+        />
     );
+
+    useEffect(() => {
+        if (showHomepage) {
+            navigation.navigate('Welcome');
+        };
+    }, [showHomepage]);
+
+    if (!showHomepage) {
+        return (
+            <AppIntroSlider 
+                data={slidesData}
+                renderItem={({ item }) => {
+                    return (
+                        <View style={styles.container}>
+                            <Image source={item.image} style={[styles.image, { width }]} contentFit='contain' />
+                            <Text style={[styles.title, {
+                                color: colors.text,
+                                fontSize: typography.fontSizes.xl
+                            }]}>
+                                {item.title}
+                            </Text>
+                            <Text style={[styles.description, {
+                                color: colors.textSecondary,
+                                fontSize: typography.fontSizes.md
+                            }]}>
+                                {item.description}
+                            </Text>
+                        </View>
+                    );
+                }}
+                activeDotStyle={{
+                    backgroundColor: colors.primary,
+                    width: 30,
+                }}
+                renderNextButton={() => buttonLabel('Siguiente', 'NextButton')}
+                showPrevButton={true}
+                renderPrevButton={() => buttonLabel('Anterior', 'PrevButton')}
+                renderDoneButton={() => buttonLabel('Listo', 'DoneButton')}
+                onDone={() => {
+                    setShowHomePage(true);
+                }}
+            />
+        );
+    };
+
+    return null;
 };
 
 const styles = StyleSheet.create({
@@ -17,5 +80,30 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    image: {
+        height: 400,
+        justifyContent: 'center',
+    },
+    title: {
+        fontWeight: '600',
+        marginBottom: 10,
+        textAlign: 'center',
+    },
+    description: {
+        textAlign: 'center',
+        paddingHorizontal: 20,
+        lineHeight: 22
+    },
+    next: {
+        paddingTop: 12,
+        paddingHorizontal: 12,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        gap: 5,
+    },
+    nextText: {
+        fontWeight: '600',
     },
 });
